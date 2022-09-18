5b69b645a4444c47dcdad5a3887e1953879488a9
Murat Torun <murattorun260@gmail.com>
2022-09-18 21:03:33 +0300

    Add UI

--- index.js
+++ index.js
@@ -10,32 +10,47 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerSelection.toLowerCase();
     if (playerSelection === computerSelection) return "It's Draw!";
     if (playerSelection === "rock") {
-        if (computerSelection === "scissors") return "You Win! Rock beats Scissors";
+        if (computerSelection === "scissors")
+            return "You Win! Rock beats Scissors";
         if (computerSelection === "paper") return "You Lose! Paper beats Rock";
     }
     if (playerSelection === "paper") {
         if (computerSelection === "rock") return "You Win! Paper beats Rock";
-        if (computerSelection === "scissors") return "You Lose! Scissors beats Paper";
+        if (computerSelection === "scissors")
+            return "You Lose! Scissors beats Paper";
     }
     if (playerSelection === "scissors") {
-        if (computerSelection === "paper") return "You Win! Scissors beats Paper";
-        if (computerSelection === "rock") return "You Lose! Rock beats Scissors";
+        if (computerSelection === "paper")
+            return "You Win! Scissors beats Paper";
+        if (computerSelection === "rock")
+            return "You Lose! Rock beats Scissors";
     }
 }
 function game() {
     let playerScore = 0;
     let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Rock, Paper or Scissors?");
-        const roundResult = playRound(playerSelection, getComputerChoice());
-        if (roundResult.includes("Win")) playerScore++;
-        if (roundResult.includes("Lose")) computerScore++;
-        // If it's a draw, no one gets a point
-        console.log(roundResult);
-    }
-    console.log(
-        `The score is ${playerScore}-${computerScore}. ${playerScore > computerScore ? "Player" : "Computer"} wins!`
-    );
-}
+    const buttons = document.querySelectorAll("button");
 
+    buttons.forEach((element) => {
+        element.addEventListener("click", () => {
+            const roundResult = playRound(element.id, getComputerChoice());
+            if (roundResult.includes("Win")) playerScore++;
+            if (roundResult.includes("Lose")) computerScore++;
+            // Nobody gets a point if it is a draw
+            document.querySelector(".round-result").textContent = roundResult;
+            document.querySelector(
+                ".score"
+            ).textContent = `${playerScore}-${computerScore}`;
+            if (playerScore === 5 || computerScore === 5) {
+                console.log(
+                    `The score is ${playerScore}-${computerScore}. ${
+                        playerScore > computerScore ? "Player" : "Computer"
+                    } wins!`
+                );
+                playerScore = 0;
+                computerScore = 0;
+            }
+        });
+    });
+}
 game();
