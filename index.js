be32cce52031d62bc976624034c9557502eaee78
Murat Torun <murattorun260@gmail.com>
2022-09-18 15:11:17 +0300

    Create game()

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) return "It's Draw!";
     if (playerSelection === "rock") {
         if (computerSelection === "scissors") return "You Win! Rock beats Scissors";
-        if (computerSelection === "Paper") return "You Lose! Paper beats Rock";
+        if (computerSelection === "paper") return "You Lose! Paper beats Rock";
     }
     if (playerSelection === "paper") {
         if (computerSelection === "rock") return "You Win! Paper beats Rock";
@@ -22,3 +22,20 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === "rock") return "You Lose! Rock beats Scissors";
     }
 }
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock, Paper or Scissors?");
+        const roundResult = playRound(playerSelection, getComputerChoice());
+        if (roundResult.includes("Win")) playerScore++;
+        if (roundResult.includes("Lose")) computerScore++;
+        // If it's a draw, no one gets a point
+        console.log(roundResult);
+    }
+    console.log(
+        `The score is ${playerScore}-${computerScore}. ${playerScore > computerScore ? "Player" : "Computer"} wins!`
+    );
+}
+
+game();
