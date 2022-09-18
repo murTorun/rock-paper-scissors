5ba1d1ce9c40987a60e0d8195d0dbffbcf78f46d
Murat Torun <murattorun260@gmail.com>
2022-09-18 14:54:12 +0300

    Create playRound()

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ function getComputerChoice() {
     if (choice === 1) return "Paper";
     if (choice === 2) return "Scissors";
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection === computerSelection) return "It's Draw!";
+    if (playerSelection === "rock") {
+        if (computerSelection === "scissors") return "You Win! Rock beats Scissors";
+        if (computerSelection === "Paper") return "You Lose! Paper beats Rock";
+    }
+    if (playerSelection === "paper") {
+        if (computerSelection === "rock") return "You Win! Paper beats Rock";
+        if (computerSelection === "scissors") return "You Lose! Scissors beats Paper";
+    }
+    if (playerSelection === "scissors") {
+        if (computerSelection === "paper") return "You Win! Scissors beats Paper";
+        if (computerSelection === "rock") return "You Lose! Rock beats Scissors";
+    }
+}
