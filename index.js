a8b77ad61852db9f7f40185d5b0c7f075eb1a841
Murat Torun <murattorun260@gmail.com>
2022-09-18 14:24:45 +0300

    Create getComputerChoice()

---
+++ index.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3);
+    if (choice === 0) return "Rock";
+    if (choice === 1) return "Paper";
+    if (choice === 2) return "Scissors";
+}
